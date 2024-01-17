26bb16c73b24c3703dd06761d1669e70e27ae82c
Jennifer Peterson <jennpeterson986@gmail.com>
2024-01-16 21:55:15 -0800

    Created Student Class

---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    public class Student
+    {
+        /// <summary>
+        ///Legal First name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///Legal First name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///Assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
